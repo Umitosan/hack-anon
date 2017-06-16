4dda82e78050fff71164c697019fed6dad85ec13
Dominic Brown <yeahdom@gmail.com>
2017-06-16 14:02:07 -0700

    add 'new member' feature

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../member.service';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [MemberService]
 })
 export class AdminComponent implements OnInit {
+  showNewMemberForm = false;
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
   }
+
+  addMemberClicked() {
+    this.showNewMemberForm = true;
+  }
+
+  submitForm(name: string, role: string, tech: string, bio: string) {
+    var newMember: Member = new Member(name, role, tech, bio);
+    this.memberService.addMember(newMember);
+
+    this.showNewMemberForm = false;
+  }
+
 
 }
