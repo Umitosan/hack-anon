7385d218bce0b77e0550c43ae8a86cdcb0a693b4
Umitosan <yeahdom@gmail.com>
2018-10-05 18:00:55 -0700

    update to angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+// general angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
+// firebase related
 import { masterFirebaseConfig } from './api-keys';
+// new firebase related
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+// components
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FrontComponent } from './front/front.component';
@@ -17,7 +21,7 @@ import { ServiceYearsPipe } from './service-years.pipe';
 import { ForumComponent } from './forum/forum.component';
 import { About2Component } from './about2/about2.component';
 
-export const firebaseConfig = {
+var firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
   authDomain: masterFirebaseConfig.authDomain,
   databaseURL: masterFirebaseConfig.databaseURL,
@@ -49,3 +53,43 @@ export const firebaseConfig = {
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+// default app.module.ts before changes
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+//
+// import { AppComponent } from './app.component';
+// import { AboutComponent } from './about/about.component';
+// import { About2Component } from './about2/about2.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { EditMemberComponent } from './edit-member/edit-member.component';
+// import { ForumComponent } from './forum/forum.component';
+// import { FrontComponent } from './front/front.component';
+// import { MemberDetailComponent } from './member-detail/member-detail.component';
+// import { RosterComponent } from './roster/roster.component';
+//
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     AboutComponent,
+//     About2Component,
+//     AdminComponent,
+//     EditMemberComponent,
+//     ForumComponent,
+//     FrontComponent,
+//     MemberDetailComponent,
+//     RosterComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
